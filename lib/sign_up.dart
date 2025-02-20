90a0c994cae54e983bc1ec21e4644dc4f9a473a7
Betelhem <Betelhemmisgan49@gmail.com>
2025-02-20 20:56:42 +0300

    Added sign-up page based on Login page

--- lib/sign_up.dart
+++ lib/sign_up.dart
@@ -1,10 +1,108 @@
 import 'package:flutter/material.dart';
 
-class SignUp extends StatelessWidget {
+class SignUp extends StatefulWidget {
   const SignUp({super.key});
 
   @override
+  State<SignUp> createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  //to manage what is inside our text field we define controllers
+  final GlobalKey<FormState> _signInKey = GlobalKey();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final RegExp emailValid = RegExp(
+      r"^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@(([0-9a-zA-Z])+([-\w]*[0-9a-zA-Z])*\.)+[a-zA-Z]{2,9})$");
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Form(
+        //alows us to validate both text fields instade of doing it indivisually
+        //to find this specific form we us key
+        key: _signInKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Sign in to Twitter",
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              margin: EdgeInsets.fromLTRB(15, 30, 15, 0),
+              decoration: BoxDecoration(
+                  color: Colors.grey.shade200,
+                  borderRadius: BorderRadius.circular(30)),
+              child: TextFormField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                    hintText: "Enter an Email",
+                    border: InputBorder.none,
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 15, horizontal: 20)),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter an email";
+                  } else if (!emailValid.hasMatch(value)) {
+                    return "Please enter a valid email";
+                  }
+                  //Regular Expresstion
+                  return null;
+                },
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              margin: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                  color: Colors.grey.shade200,
+                  borderRadius: BorderRadius.circular(30)),
+              child: TextFormField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                  hintText: "Enter a Password",
+                  border: InputBorder.none,
+                ),
+                //define what is valid for password (value) -what is currently in text field
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter a password";
+                  } else if (value.length < 6) {
+                    return "Pssword must be at least 6 characters";
+                  }
+                  return null;
+                },
+              ),
+            ),
+            Container(
+              width: 250,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(30)),
+              child: TextButton(
+                  onPressed: () {
+                    if (_signInKey.currentState!.validate()) {
+                      debugPrint("Email: $_emailController");
+                      debugPrint("Password: $_passwordController");
+                    }
+                  },
+                  child: Text(
+                    "Sign Up",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  )),
+            ),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text("Already have an account? Log in"))
+          ],
+        ),
+      ),
+    );
   }
 }
