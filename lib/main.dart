9c80af03db841cb81f7f705adcce95896fe313fa
Betelhem <Betelhemmisgan49@gmail.com>
2025-02-26 08:35:41 +0300

    initialize firebase in the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:twitter_clone/sign_up.dart';
+import 'package:twitter_clone/firebase_options.dart';
 
-void main() {
+import 'package:twitter_clone/pages/sign_in.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -12,123 +16,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  //to manage what is inside our text field we define controllers
-  final GlobalKey<FormState> _signInKey = GlobalKey();
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  final RegExp emailValid = RegExp(
-      r"^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@(([0-9a-zA-Z])+([-\w]*[0-9a-zA-Z])*\.)+[a-zA-Z]{2,9})$");
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Form(
-        //alows us to validate both text fields instade of doing it indivisually
-        //to find this specific form we us key
-        key: _signInKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-             FaIcon(
-              FontAwesomeIcons.twitter,
-              size: 70,
-              color: Colors.blue,
-            ),
-            SizedBox(
-              height: 20
-            ),
-            Text(
-              "Log in to Twitter",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 10),
-              margin: EdgeInsets.fromLTRB(15, 30, 15, 0),
-              decoration: BoxDecoration(
-                  color: Colors.grey.shade200,
-                  borderRadius: BorderRadius.circular(30)),
-              child: TextFormField(
-                controller: _emailController,
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                    hintText: "Enter an Email",
-                    border: InputBorder.none,
-                    contentPadding:
-                        EdgeInsets.symmetric(vertical: 15, horizontal: 20)),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Please enter an email";
-                  } else if (!emailValid.hasMatch(value)) {
-                    return "Please enter a valid email";
-                  }
-                  //Regular Expresstion
-                  return null;
-                },
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.symmetric(vertical: 10),
-              margin: EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                  color: Colors.grey.shade200,
-                  borderRadius: BorderRadius.circular(30)),
-              child: TextFormField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-                  hintText: "Enter a Password",
-                  border: InputBorder.none,
-                ),
-                //define what is valid for password (value) -what is currently in text field
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Please enter a password";
-                  } else if (value.length < 6) {
-                    return "Pssword must be at least 6 characters";
-                  }
-                  return null;
-                },
-              ),
-            ),
-            Container(
-              width: 250,
-              decoration: BoxDecoration(
-                  color: Colors.blue, borderRadius: BorderRadius.circular(30)),
-              child: TextButton(
-                  onPressed: () {
-                    if (_signInKey.currentState!.validate()) {
-                      debugPrint("Email: $_emailController");
-                      debugPrint("Password: $_passwordController");
-                    }
-                  },
-                  child: Text(
-                    "Log In",
-                    style: TextStyle(color: Colors.white, fontSize: 18),
-                  )),
-            ),
-            TextButton(
-                onPressed: () {
-                  Navigator.of(context)
-                      .push(MaterialPageRoute(builder: (context) => SignUp()));
-                },
-                child: Text("Don't have an account? Sign up here"))
-          ],
-        ),
-      ),
+      home: SignInPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
